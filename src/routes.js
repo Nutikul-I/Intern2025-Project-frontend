4daf69f581f15f2cefba30afb95f6f34ff348187
Tipyadaa <amyadaarttywork@gmail.com>
2025-05-08 11:40:12 +0700

    update route orderhistory

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import OrderHistory from "./customers/components/OrderHistory";
 import Loading from "./page/loading";
 import Login from "./page/login";
+import Layout from  "../src/customers/pages/Layout";
+import ProcessTwo from "./customers/components/ProcessTwo";
+import ProcessThree from "./customers/components/ProcessThree";
+import LayoutAdmin from "./employees/pages/Layout";
+import EmployeePage from "./employees/components/Employee";
+
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/" element={<Loading />} />
     <Route path="/login" element={<Login />} />
+
+    <Route element={<Layout/>}>
+      <Route path="/" element={<Loading />} />
+      <Route path="/orderhistory" element={<OrderHistory />} />
+      <Route path="/processtwo" element={<ProcessTwo />} />
+      <Route path="/processtree" element={<ProcessThree  />} />
+    </Route>
+
+    <Route element={< LayoutAdmin / >}>
+      <Route path="/" element={<Loading />} />
+      <Route path="/employeePage" element={<EmployeePage />} />
+  
+    </Route>
+
+
   </Routes>
+
 );
+
+
+
+
+
 
 export default AppRoutes;
