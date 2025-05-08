ac1750f8f4c4c8e12448ccef11f5d46a13181b45
loywizzy <kdftvguitarandarmy@gmail.com>
2025-05-08 15:51:02 +0700

    fix: resolve routing issues in AppRoutes; correct component imports and add ProductDetail route

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OrderHistory from "./customers/components/OrderHistory";
 import Loading from "./page/loading";
 import Login from "./page/login";
-<<<<<<< HEAD
+
 import HomePage from "./customers/components/HomePage"
 import Layout from "./customers/pages/Layout";
 import Register from "./page/register";
-=======
-import Layout from  "../src/customers/pages/Layout";
 import ProcessTwo from "./customers/components/ProcessTwo";
+import ProductDetail from "./customers/components/ProductDetail";
 
->>>>>>> 5b49138682e2f422fad0443eb839657f3d085cba
+
 
 const AppRoutes = () => (
   <Routes>
@@ -20,8 +19,11 @@ const AppRoutes = () => (
 
     <Route element={<Layout/>}>
       <Route path="/" element={<Loading />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/orderhistory" element={<OrderHistory />} />
-      <Route path="/processtwo" element={<ProcessTwo />} />
+      <Route path="/processtwo" element={<ProcessTwo />} 
+      <route path="/productdetail/:id" element={<ProductDetail />}
+      />
       
     </Route>
 
