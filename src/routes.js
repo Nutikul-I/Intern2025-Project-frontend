fbaee0ef211fc954350520d3e070dc84ee426f68
Tipyadaa <amyadaarttywork@gmail.com>
2025-05-08 11:40:52 +0700

    update route orderhistory

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,7 @@ import OrderHistory from "./customers/components/OrderHistory";
 import Loading from "./page/loading";
 import Login from "./page/login";
 import Layout from  "../src/customers/pages/Layout";
-import ProcessTwo from "./customers/components/ProcessTwo";
-import ProcessThree from "./customers/components/ProcessThree";
-import LayoutAdmin from "./employees/pages/Layout";
-import EmployeePage from "./employees/components/Employee";
+
 
 
 const AppRoutes = () => (
@@ -17,14 +14,6 @@ const AppRoutes = () => (
     <Route element={<Layout/>}>
       <Route path="/" element={<Loading />} />
       <Route path="/orderhistory" element={<OrderHistory />} />
-      <Route path="/processtwo" element={<ProcessTwo />} />
-      <Route path="/processtree" element={<ProcessThree  />} />
-    </Route>
-
-    <Route element={< LayoutAdmin / >}>
-      <Route path="/" element={<Loading />} />
-      <Route path="/employeePage" element={<EmployeePage />} />
-  
     </Route>
 
 
