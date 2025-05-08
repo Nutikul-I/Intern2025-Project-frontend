2e16eb409edf03d1f576e86ce9fe93ffa1625a24
SORASONATA <aunny.020.aunny@gmail.com>
2025-05-08 16:44:46 +0700

    Resolve merge conflict in routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,22 @@ import Login from "./page/login";
 import HomePage from "./customers/components/HomePage"
 import Layout from "./customers/pages/Layout";
 import AllProduct from "./customers/components/AllProduct";
+import OrderHistory  from "./customers/components/OrderHistory";
+import ProcessTwo    from "./customers/components/ProcessTwo";
+import ProductDetail from "./customers/components/ProductDetail";
+import Register from "./page/register";
+
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/login" element={<Login />} />
-
+    <Route path="/register" element={<Register />} />
     <Route element={<Layout />}>
       <Route path="/" element={<Loading />} />
       <Route path="home" element={<HomePage />} />
+      <Route path="orderhistory" element={<OrderHistory />} />
+      <Route path="processtwo" element={<ProcessTwo />} />
+      <Route path="productdetail/:id" element={<ProductDetail />} />
       <Route path="allproduct" element={<AllProduct />} />
     </Route>
 
