33992b78e91cfb5a9b765ea0ef0db6dc0f3fa09e
loywizzy <kdftvguitarandarmy@gmail.com>
2025-05-09 11:31:51 +0700

    refactor: clean up imports and improve route definitions in AppRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import OrderHistory from "./customers/components/OrderHistory";
-import Loading from "./page/loading";
-import Login from "./page/login";
+import { Routes, Route } from "react-router-dom";
 
-import HomePage from "./customers/components/HomePage"
-import Layout from "./customers/pages/Layout";
-import Register from "./page/register";
+import OrderHistory from "./customers/components/OrderHistory";
 import ProcessTwo from "./customers/components/ProcessTwo";
 import ProductDetail from "./customers/components/ProductDetail";
-
-
+import HomePage from "./customers/components/HomePage";
+import Layout from "./customers/pages/Layout";
+import Loading from "./page/loading";
+import Login from "./page/login";
+import Register from "./page/register";
+import AllProduct from "./customers/components/AllProduct";
+import ShoppingCart from "./customers/components/ShoppingCart";
+import EditInfo from "./customers/components/EditInfo";
+import CheckoutPage from "./customers/components/CheckoutPage";
+import LayoutEmployee from "./employees/pages/Layout";
+import DashBoard from "./employees/components/Dashboard";
 
 const AppRoutes = () => (
   <Routes>
-
-    <Route path="/login"    element={<Login />} />
+    <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
 
-    <Route element={<Layout/>}>
+    <Route element={<Layout />}>
+      <Route index element={<Loading />} />
       <Route path="/" element={<Loading />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/orderhistory" element={<OrderHistory />} />
-      <Route path="/processtwo" element={<ProcessTwo />} />
-      <route path="/productdetail/:id" element={<ProductDetail />}
-      />
-      
+      <Route path="home" element={<HomePage />} />
+      <Route path="orderhistory" element={<OrderHistory />} />
+      <Route path="product/:id" element={<ProductDetail />} />
+      <Route path="allproduct" element={<AllProduct />} />
+      <Route path="shoppingcart" element={<ShoppingCart />} />
+      <Route path="editinfo" element={<EditInfo />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="process" element={<ProcessTwo />} />
     </Route>
-
+    
+    <Route element={<LayoutEmployee />}>
+      <Route path="/employee" element={<Loading />} />
+      <Route path="/employee/dashboard" element={<DashBoard />} />
+    </Route>
   </Routes>
-
 );
 
 export default AppRoutes;
